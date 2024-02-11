9d01318e38b1f1e1de2cc5841c972ac65809a0b5
İnanç Eren Demir <inancdemir@gmail.com>
2024-02-11 14:31:45 +0300

    11/02

--- assets/quick-look.js
+++ assets/quick-look.js
@@ -39,24 +39,3 @@ document.querySelectorAll('.modal-close, .quick-view-product').forEach((el) => {
     document.getElementById('quick-view-modal').classList.remove('active');
   });
 });
-
-/*
-document.getElementById('quick-view-close').addEventListener('click', () => {
-  let img_area = document.getElementById('ql-product-image-area');
-  let title_area = document.getElementById('ql-product-title');
-  //let desc_area = document.getElementById('ql-description');
-  let optionsContainer = document.getElementById('options-container');
-  const originalPriceSpan = document.querySelector('.original-price');
-  const discountedPriceSpan = document.querySelector('.discounted-price');
-  // Reset the content of modified elements to their initial state
-  img_area.innerHTML = ''; // Clear the product image
-  title_area.innerHTML = ''; // Clear the product title
-  //desc_area.innerHTML = ''; // Clear the product description
-  optionsContainer.innerHTML = ''; // Clear the product options
-  originalPriceSpan.innerHTML = ''; // Clear the product options
-  discountedPriceSpan.innerHTML = ''; // Clear the product options
-
-  // Toggle the visibility of the quick view modal
-  document.getElementById('quick-view-modal').classList.toggle('active');
-});
-*/
