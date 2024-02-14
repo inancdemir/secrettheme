3b7abf504036a34e841eda7ab3788552d03343b6
İnanç Eren Demir <inancdemir@gmail.com>
2024-02-14 10:07:48 +0300

    14/02

--- assets/quick-look.js
+++ assets/quick-look.js
@@ -1,20 +1,22 @@
 /*quickview*/
-var productQL = document.querySelectorAll('.quick-view-button');
-productQL.forEach(function (item, index) {
-  item.addEventListener(
-    'click',
-    function (e) {
-      var product_url = this.getAttribute('data-product-id');
-      console.log(product_url);
-      let p_response = fetch(product_url)
-        .then((response) => response.json())
-        .then((data) => {
-          showquickView(data);
-        });
-    },
-    false
-  );
+
+document.addEventListener('click', function(event) {
+  var target = event.target.closest('.quick-view-button');
+  if (target) {
+    var product_url = target.getAttribute('data-product-id');
+
+    let p_response = fetch(product_url)
+      .then((response) => response.json())
+      .then((data) => {
+        showquickView(data);
+      })
+      .catch(error => {
+        console.error('ERROR: Quick look is not working. Please let the developer theme now.:', error);
+      });
+  }
 });
+
+
 
 document.querySelector('.modal-content').addEventListener('click', (e) => {
   e.stopPropagation();
@@ -27,6 +29,7 @@ document.querySelectorAll('.modal-close, .quick-view-product').forEach((el) => {
     let optionsContainer = document.getElementById('options-container');
     const originalPriceSpan = document.querySelector('.original-price');
     const discountedPriceSpan = document.querySelector('.discounted-price');
+    const add = document.querySelector('#add-to-cart');
     // Reset the content of modified elements to their initial state
     img_area.innerHTML = ''; // Clear the product image
     title_area.innerHTML = ''; // Clear the product title
@@ -34,7 +37,7 @@ document.querySelectorAll('.modal-close, .quick-view-product').forEach((el) => {
     optionsContainer.innerHTML = ''; // Clear the product options
     originalPriceSpan.innerHTML = ''; // Clear the product options
     discountedPriceSpan.innerHTML = ''; // Clear the product options
-
+    add.innerHTML = '';
     // Toggle the visibility of the quick view modal
     document.getElementById('quick-view-modal').classList.remove('active');
   });
